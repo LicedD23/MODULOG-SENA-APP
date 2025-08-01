28a8665e4c32cdc4cc92329c5de0bdb091e2d7c6
Liced Dayana <dayadiz.23@gmail.com>
2025-08-01 11:18:41 -0500

    creacion de nuevos modelos

--- SENA_APP/aprendices/models.py
+++ SENA_APP/aprendices/models.py
@@ -9,7 +9,8 @@ class Aprendiz(models.Model):
     correo=models.EmailField()
     fecha_nacimiento=models.DateField()
     ciudad=models.CharField(max_length=100)
-    programa=models.CharField(max_length=100,null=True)
+    #programa=models.CharField(max_length=100,null=True)
+    
     
     def __str__(self):
         return f"{self.nombre} {self.apellido} - {self.documento_identidad}"
