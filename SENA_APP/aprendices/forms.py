11901635c57597023af65eade055c841d77e9af6
Liced Dayana Diaz Mayorga <dayadiz.23@gmail.com>
2025-08-13 15:39:10 -0500

    creacion de agregar aprendiz

--- SENA_APP/aprendices/forms.py
+++ SENA_APP/aprendices/forms.py
@@ -1,50 +1,49 @@
 from django import forms
 from .models import Aprendiz
 
-class AprendizForm(forms.Form):
-    documento_identidad = forms.CharField(max_length=20, label="Documento  de Identidad")
-    nombre = forms.CharField(max_length=100, label="Nombre")
-    apellido=forms.CharField(max_length=100, label="Apellido")
-    telefono = forms.CharField(max_length=10, label="Telefono")
-    correo = forms.EmailField(label="Correo Electronico")
-    fecha_nacimiento =forms.DateField(label="Fecha de Nacimiento")
-    ciudad = forms.CharField(max_length=100, required=False,label="Ciudad")
 
+class AprendizForm(forms.Form):
+    documento_identidad = forms.CharField(max_length=20, label="Documento de Identidad")
+    nombre = forms.CharField(max_length=100, label="Nombre")
+    apellido = forms.CharField(max_length=100, label="Apellido")
+    telefono = forms.CharField(max_length=10, label="Teléfono")
+    correo = forms.EmailField(label="Correo Electrónico")
+    fecha_nacimiento = forms.DateField(label="Fecha de Nacimiento")
+    ciudad = forms.CharField(max_length=100, required=False, label="Ciudad")
+    
+    
+    #Validaciones personalizadas 
     def clean(self):
-        cleaned_data= super().clean()
-        documento= cleaned_data.get('documento_identidad')
+        cleaned_data = super().clean()
+        documento = cleaned_data.get('documento_identidad')
         nombre = cleaned_data.get('nombre')
         apellido = cleaned_data.get('apellido')
-        
+
         if not documento or not nombre or not apellido:
-            raise forms.ValidationError("Todos los campos obligatorios.")
+            raise forms.ValidationError("Todos los campos son obligatorios.")
+
         return cleaned_data
     
     def clean_documento_identidad(self):
-    
-        documento =self.cleaned_data['documento_identidad']
+        documento = self.cleaned_data['documento_identidad']
         if not documento.isdigit():
-            raise forms.ValidationError("El documento  debe contener solo numeros.")
+            raise forms.ValidationError("El documento debe contener solo números.")
         return documento
-    
+
     def clean_telefono(self):
-        telefono= self.cleaned_data.get('telefono')
+        telefono = self.cleaned_data.get('telefono')
         if telefono and not telefono.isdigit():
-            raise forms.ValidationError("El  telefono debe contener solo numeros.")
+            raise forms.ValidationError("El teléfono debe contener solo números.")
         return telefono
     
-    def  save(self):
+    #Crear un método para guardar los datos del formulario en la base de datos
+    def save(self):
         Aprendiz.objects.create(
-            documento_identidad= self.cleaned_data['documento_identidad'],
+            documento_identidad=self.cleaned_data['documento_identidad'],
             nombre=self.cleaned_data['nombre'],
             apellido=self.cleaned_data['apellido'],
-            telefono=self.cleaned_data('telefono'),
-            correo =self.cleaned_data('correo'),
-            fecha_nacimieno=self.cleaned_data['fecha_nacimiento'],
+            telefono=self.cleaned_data.get('telefono'),
+            correo=self.cleaned_data.get('correo'),
+            fecha_nacimiento=self.cleaned_data['fecha_nacimiento'],
             ciudad=self.cleaned_data.get('ciudad')
         )
-            
-
-        
-    
-    
