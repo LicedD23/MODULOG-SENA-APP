978bb620cb3f1ee99366c066f66e1c01199852f1
Liced Dayana <dayadiz.23@gmail.com>
2025-08-19 10:50:49 -0500

    creacion del modelo

--- SENA_APP/asistencias/models.py
+++ SENA_APP/asistencias/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Asistencia(models.Model):
+    ESTADO_CHOICES = [
+        ('SI','asistio'),
+        ('NO','no asistio'),
+    ]
+    Codigo= models.ForeignKey('programas.Programa', on_delete=models.CASCADE ,verbose_name="Código del Programa")
+    aprendiz= models.ForeignKey('aprendices.Aprendiz', on_delete=models.CASCADE, verbose_name="Aprendiz")
+    instructor= models.ForeignKey('instructores.Instructor', on_delete=models.CASCADE, verbose_name="Instructor",help_text="Instructor que dicto la clase")
+    cantidad_horas= models.PositiveIntegerField(verbose_name="Horas de Asistencia" , help_text="Cantidad de horas de asistencia del aprendiz")
+    observaciones= models.TextField(blank=True, null=True, verbose_name="Observaciones",help_text="Observaciones sobre la asistencia")
+    fecha_inicio= models.DateField(verbose_name="Fecha Inicio")
+    fecha_fin= models.DateField(verbose_name="Fecha Fin")
+    Estado= models.CharField(max_length=2, choices=ESTADO_CHOICES, default='SI', verbose_name="Estado")
+    
+    def __str__(self):
+        return f"{self.Codigo} - {self.aprendiz}"
+    
+    class Meta:
+        verbose_name = "Asistencia"
+        verbose_name_plural = "Asistencias"
